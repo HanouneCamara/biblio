1080a2c063af581b907970009b345fe4eed3ea7c
HanouneCamara <Alicmr13@gmail.com>
2025-07-02 23:50:03 +0100

    set,check-password

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
 from flask_login import UserMixin
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import Integer, String
-from app import db  
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 
 #Modèle Utilisateur
@@ -15,3 +17,11 @@ class Utilisateur(UserMixin, db.Model):
     
     def __repr__(self) -> str:
         return f"<Utilisateur {self.username}>"
+    
+    #Hacher et stocker
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+       
+    #vérifier un mot de passe donné
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
